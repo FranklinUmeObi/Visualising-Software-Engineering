4340f42908ea6cf050a8e746bf3a759712b7d7e2
Franklin Ume Obiekwe <franklinumeobi@gmail.com>
2020-11-16 16:50:07 +0000

    started social graph

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
-//URL Endpoints
-let urlMyRepos = "https://api.github.com/users/franklinumeobi/repos";
-
-
-main(urlMyRepos)
+main()
 
 
 //------------------------------------------------------------------------------
 //Main
 //------------------------------------------------------------------------------
-async function main(url1) 
+async function main() 
 {
-    //num repos
-    let myReposData = await GetRequest(url1).catch(error => console.error(error));
-    let num = myReposData.length
+    //URL Endpoints
+    let urlMyRepos = "https://api.github.com/users/franklinumeobi/repos";
+    let urlProject = "https://api.github.com/repos/facebook/react/contributors";
+
+    //let myReposData = await GetRequest(urlMyRepos).catch(error => console.error(error));
+    //let num = myReposData.length
     //console.log(num);
     //console.log(myReposData);
 
-    let commits = await commitsPerRepo(myReposData)
+
+    //let commits = await commitsPerRepo(myReposData)
     //console.log(commits); // array of objects {repoName, numCommits}
-    pieChartCommits(commits)
+    //D3_pieChartCommits(commits)
+
+
+    let reactData = await GetRequest(urlProject).catch(error => console.error(error));
+    let nodes = []
+    let links = []
+    socialGraphParse(reactData, nodes, links)
+    D3_socialGraph(nodes, links)
 }
 
 
+//------------------------------------------------------------------------------
+//Charts
+//------------------------------------------------------------------------------
+function D3_socialGraph(nodeData, linkData){
+    //https://observablehq.com/@d3/force-directed-graph
+}
 
-function pieChartCommits(myData) 
+
+function D3_pieChartCommits(myData) 
 {
     //var data = [2, 4, 8, 10];
     var data = []
@@ -37,7 +51,8 @@ function pieChartCommits(myData)
         radius = Math.min(width, height) / 2,
         g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    var color = d3.scaleOrdinal(['#008080','#00FFFF','#4682B4','#FFFFE0','#E6E6FA', '#EE82EE','#9370DB','#4B0082','#ADFF2F','#00FF7F']);
+    var color = d3.scaleOrdinal(['#008080','#00FFFF','#4682B4','#FFFFE0','#E6E6FA', 
+    '#EE82EE','#9370DB','#4B0082','#ADFF2F','#00FF7F','#00BFFF','#4169E1','#BC8F8F','#191970']);
 
     // Generate the pie
     var pie = d3.pie();
@@ -84,7 +99,22 @@ async function commitsPerRepo(userReposData)
     return commits
 }
 
+async function socialGraphParse(rawData, nodes, links)
+{
+    console.log(rawData);
+    let arrPeople = [];
+    for (let i = 0; i < rawData.length; i++) 
+    {
+        const element = rawData[i];
+        let a = {"index":i, "username":element.login,"contributions":element.contributions, "followersUrl":element.followers_url}
+        arrPeople.push(a)
+    }
+    console.log(arrPeople);
 
+    let followers = await GetRequest(`${arrPeople[0].followersUrl}`).catch(error => console.error(error))
+    console.log(followers);
+    
+}
 
 
 
@@ -94,6 +124,6 @@ async function commitsPerRepo(userReposData)
 async function GetRequest(url) {
   const response = await fetch(url);
   let data = await response.json();
-  console.log("Get from "+ url + " is a "+response.status);
+  //console.log("Get from "+ url + " is a "+response.status);
   return data;
 }
