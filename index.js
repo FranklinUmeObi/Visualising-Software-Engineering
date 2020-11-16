9f6e53adb9577ba567bf157863fa074152f47c9b
Franklin Ume Obiekwe <franklinumeobi@gmail.com>
2020-11-16 13:32:18 +0000

    styled carousel + finished first parsed data from github

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 //URL Endpoints
 let urlMyRepos = "https://api.github.com/users/franklinumeobi/repos";
 
+
 main(urlMyRepos)
 
 
-
+//------------------------------------------------------------------------------
+//Main
+//------------------------------------------------------------------------------
 async function main(url1) 
 {
+    //num repos
     let myReposData = await GetRequest(url1).catch(error => console.error(error));
+    let num = myReposData.length
+    console.log(num);
     console.log(myReposData);
+
+    let commits = await commitsPerRepo(myReposData)
+    console.log(commits); // array of objects {repoName, numCommits}
+
 }
 
 
@@ -17,13 +27,29 @@ async function main(url1)
 
 
 
+//------------------------------------------------------------------------------
+//Data Parse Functions
+//------------------------------------------------------------------------------
+
+async function commitsPerRepo(userReposData)
+{
+    let commits = []
+    for (let i = 0; i < userReposData.length; i++) 
+    {
+        const repo = userReposData[i].name
+        let a = await GetRequest(`https://api.github.com/repos/franklinumeobi/${repo}/commits`).catch(error => console.error(error))
+        let b = {"repo":repo, "commits":a.length}
+        commits.push(b)
+    }
+    return commits
+}
 
 
 
 
 
 //------------------------------------------------------------------------------
-//Get My Repos
+//Get Request
 //------------------------------------------------------------------------------
 async function GetRequest(url) {
   const response = await fetch(url);
