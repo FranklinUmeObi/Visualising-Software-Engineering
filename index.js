062c8bee9691413555ddc565cf4620f182d29d57
Franklin Ume Obiekwe <franklinumeobi@gmail.com>
2020-11-17 17:08:41 +0000

    Finished TreeMap

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 
-//let input = "facebook"
-//let input = "franklinumeobi"
-//let input = "MunGell"
-//let input = "henrym2"
-
 function handleInput()
 {
   var x = document.getElementById("textBox").value;
@@ -35,9 +30,10 @@ async function main(user) {
   
   //Social Graph
   socialGraphParse(reposData);
-
   //PieChart -- Make this Treemap
   commitsPerRepo(reposData, user)
+  //TreeMap
+  usersLanguages(reposData, user)
 }
 
 
@@ -57,6 +53,64 @@ async function commitsPerRepo(userReposData, user) {
     commits.push(b);
   }
   D3_pieChartCommits(commits)
+}
+
+async function usersLanguages(userReposData, user) {
+  let languages = new Set();
+  let repoLangObj = []
+  for (let i = 0; i < userReposData.length; i++) 
+  {
+    const repo = userReposData[i].name;
+    let a = await GetRequest(`https://api.github.com/repos/${user}/${repo}/languages`).catch((error) => console.error(error));
+    repoLangObj.push(a)
+  }
+
+  console.log(repoLangObj);//array of repos lang objects
+
+  for (let i = 0; i < repoLangObj.length; i++) 
+  {
+    let keyArr = Object.keys(repoLangObj[i]);
+      for (let j = 0; j < keyArr.length; j++) 
+      {
+        languages.add(keyArr[j])
+      }
+  }
+  console.log(languages);//array of languages used
+
+  let langSize = new Map()
+  for (let value of languages) langSize.set(value, 0);   // init map
+
+  console.log(langSize);//array of languages used
+
+  for (let i = 0; i < repoLangObj.length; i++) 
+  {
+    let obj = repoLangObj[i]
+    for (const [key, value] of Object.entries(obj)) {
+
+      let oldVal = langSize.get(key);
+      let newVal = value + oldVal
+      const newMap = langSize.set(key, newVal)
+      langSize = newMap
+    }
+
+  }
+  console.log(langSize);//array of languages used
+
+  let children = []
+  for (const [key, value] of langSize.entries()) 
+  {
+    let node = {
+      "name":key,
+      "group":"A",
+      "value":value,
+      "colname":"languages"
+    }
+    children.push(node)
+
+  }
+  console.log(children);
+
+  D3_TreeMap(children);
 }
 
 
@@ -238,3 +292,55 @@ function D3_pieChartCommits(myData) {
     .attr("d", arc);
 }
 
+
+
+
+
+function D3_TreeMap(childrenData){
+  var svg = d3.select(".chart3");
+  svg.selectAll("*").remove()
+  var width = svg.attr("width");
+  var height = svg.attr("height");
+  var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+  var data = {
+    name: "Languages",
+    children: childrenData
+  };
+
+
+  // Give the data to this cluster layout:
+  var root = d3.hierarchy(data).sum(function(d){ return d.value}) // Here the size of each leave is given in the 'value' field in input data
+
+  // Then d3.treemap computes the position of each element of the hierarchy
+  d3.treemap()
+    .size([width, height])
+    .padding(2)
+    (root)
+
+  // use this information to add rectangles:
+  svg
+    .selectAll("rect")
+    .data(root.leaves())
+    .enter()
+    .append("rect")
+      .attr('x', function (d) { return d.x0; })
+      .attr('y', function (d) { return d.y0; })
+      .attr('width', function (d) { return d.x1 - d.x0; })
+      .attr('height', function (d) { return d.y1 - d.y0; })
+      .style("stroke", "black")
+      .attr("fill", function (d, i) {
+        return color(i);
+      })
+  // and to add the text labels
+  svg
+    .selectAll("text")
+    .data(root.leaves())
+    .enter()
+    .append("text")
+      .attr("x", function(d){ return d.x0+5})    // +10 to adjust position (more right)
+      .attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
+      .text(function(d){ return d.data.name })
+      .attr("font-size", "15px")
+      .attr("fill", "black")
+}
